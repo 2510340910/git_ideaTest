db6cc955fa04dbb8b11ada6125c3f499d15046eb
ZHJ <ZHJ@qq.com>
2022-08-27 10:33:47 +0800

    use IDEA first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-27 10:29
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git！！！");
+    }
+}
